add77c005710b047c4c523f47fb171174aa6f357
tgortworst <t.gortworst@tm-pro.eu>
2021-12-31 16:11:34 +0100

    swap mute and play

--- keyboards/gmmk/pro/ansi/keymaps/timGW/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/timGW/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //      Sh_L              Z        X        C        V            B        N        M        ,        .        ?                 Sh_R     Up       End
     //      Ct_L     Win_L    Alt_L                                   SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
 	[_BASE] = LAYOUT(
-	        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   SCMD(KC_R), KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  SCMD(KC_4),       KC_MUTE,
+	        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   SCMD(KC_R), KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  SCMD(KC_4),       KC_MPLY,
 	        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
 	        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
 	        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGDN,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //      ▼                 ▼        ▼        ▼        ▼         ▼        ▼        ▼        ▼        ▼        ▼                 ▼        RGB+      ▼
     //      ▼        ▼        ▼                                    ▼                                   ▼        ▼        ▼        RGB_EFF- RGB+      RGB_EFF+
     [_FN1] = LAYOUT(
-            RGB_TOG, RGB_SAD, RGB_SAI, RGB_HUD, RGB_HUI, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_MPLY,
+            RGB_TOG, RGB_SAD, RGB_SAI, RGB_HUD, RGB_HUI, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_MUTE,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD, RGB_VAI, KC_TRNS,           KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,             KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,
